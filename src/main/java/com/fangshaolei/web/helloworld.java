fc3cc78eb111358e01710a6bd04454ce35a1650d
lao6 <sl.fang@qq.com>
2022-03-10 16:52:16 +0800

    first commit!

---
+++ src/main/java/com/fangshaolei/web/helloworld.java
@@ -0,0 +1,14 @@
+package com.fangshaolei.web;
+
+/**
+ * @author fangshaolei
+ * @version 1.0.0
+ * @ClassName helloworld
+ * @Description 这是一个test
+ * @createTime 2022/03/10 16:38
+ **/
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
---
+++ src/main/java/com/fangshaolei/web/test.java
@@ -0,0 +1,15 @@
+package com.fangshaolei.web;
+
+/**
+ * @author fangshaolei
+ * @version 1.0.0
+ * @ClassName test
+ * @Description 这是一个test的文件夹
+ * @createTime 2022/03/10 16:33
+ **/
+public class test {
+    public static void main(String[] args) {
+
+
+    }
+}
